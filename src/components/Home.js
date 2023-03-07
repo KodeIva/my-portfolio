7e1312c6b3646ba348dffdb20c09d4dfc3660a7e
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-03-07 17:42:16 +0000

    Add changes to home component, add data.json file and  images folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Home() {
   return(
-   <div>
+   <div className='home'>
        <h1>CodeIva</h1>
      <h3>Web Developer in development!</h3>
      <h3>Welcome to my Portfolio Page</h3>
