87685468a273b7cd334cd905450f153a220167a9
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-03-10 15:49:13 +0000

    Created Home.css, more styling added and transfered

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import '../Styles/home.css'
 
 
 export default function Home() {
   return(
    <div className='home'>
-   <div>
-      <img src="https://cdn.pixabay.com/photo/2016/05/26/14/29/woman-1417267_960_720.png" width="400" alt="" />
-   </div>
-   <div>
-     <h1>CodeIva</h1>
-     <h3>Web Developer in development!</h3>
-     <h3>Welcome to my Portfolio Page</h3>
-   </div>
-  
+    <div>
+      <img className='photo' src="https://cdn.pixabay.com/photo/2016/05/26/14/29/woman-1417267_960_720.png" width="400" alt="" />
+    </div>
+    <div>
+     <h1 className='brand'>CodeIva</h1>
+     <h3 className='statement'>Web Developer in development !!!</h3>
+     <h3 className='statement1'>Welcome to my Portfolio Page</h3>
+    </div>
    </div>
 
   )
