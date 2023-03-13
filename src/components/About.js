517ae4da77af7ab0f12d1aea817e50579fec408c
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-03-13 13:48:15 +0000

    About.js content added, about.css mobile changes added

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,23 @@ import '../Styles/about.css'
 
 export default function About() {
  return (
-  <div className='about'>
-
-    <p>I am an enthusiastic, self-taught, motivated Web Developer in development, who is looking for a career change from aviation and willing to become a professional in a digital world. After realising my passion about creating things on my own and making it work made me really driven to get involved into programing environment. I am keen to transform my basic knowledge into professional skills, and looking for a Junior Developer position that will offer opportunity to learn and continue my further development under the mentorship of professionals.</p>
+  <section className="aboutMe">
+   <div className='about'>
+    <h1>About Me</h1>
+     <p>I am an enthusiastic, self-taught, motivated Web Developer in development, who is looking for a career change from aviation and willing to become a professional in a digital world.</p>
+     <p> I am keen to transform my basic knowledge into professional skills, and looking for a Junior Developer position that will offer opportunity to learn and continue my further development under the mentorship of professionals.</p>
   </div>
+  <div className="skills">
+    <h1>My Skills</h1>
+      <p>&#x2022; HTML</p>
+      <p>&#x2022; CSS</p>
+      <p>&#x2022; JavaScript</p>
+      <p>&#x2022; React</p>
+      <p>&#x2022; jQuery</p>
+      <p>&#x2022; Bootstrap</p>
+      <p>&#x2022; Git / GitHub</p>  
+  </div>
+</section>
+
  )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,7 +19,6 @@ export default function Project () {
     </div>
      )
     })}
-   
    </section>
   </>
  )
